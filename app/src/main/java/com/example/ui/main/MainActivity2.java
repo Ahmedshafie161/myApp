aa18c8193ac2d0f15e6a528df932ac7859228e1e
Ahmed Shafie <Ahmed.elmenshawie@gmail.com>
2022-03-06 15:01:32 -0600

    Firebase write code ,url

--- app/src/main/java/com/example/ui/main/MainActivity2.java
+++ app/src/main/java/com/example/ui/main/MainActivity2.java
@@ -25,6 +25,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,10 +33,15 @@ import android.widget.Toast;
 import com.example.BlankFragment;
 import com.example.BlankFragment2;
 import com.example.R;
+import com.example.data.MyDatabase;
 import com.example.onItemClickListener;
 import com.example.pojo.MyAggregateData;
 import com.example.pojo.PostModel;
 import com.example.ui.AddDataActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,6 +75,8 @@ public class MainActivity2 extends AppCompatActivity implements BlankFragment2.M
         for (int i =0; i<50 ; i++){
             al.add(new MyAggregateData("ahmed",1,10));
         }
+        MyDatabase myDatabase= new MyDatabase();
+        myDatabase.addToDatabase(this,"ali","kareem");
 
         //MVVM implemntation
         postViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
